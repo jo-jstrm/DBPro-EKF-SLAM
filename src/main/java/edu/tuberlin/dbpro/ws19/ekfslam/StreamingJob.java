f289bcd642b6e3d7ffa46d41335b61dbc3101902
beneo96 <benedikt.livert@gmx.de>
2019-11-27 22:21:21 +0100

    Added Sink for Filtered sensor Data (lat, long, steering)
    Also implemented DiscreteKalmanFunction, which should be a simple form of a Discrete Kalman Function.
    
    Measurements are the sensor readings and predictions happen according to some equation of the victoriaPark Paper

--- src/main/java/edu/tuberlin/dbpro/ws19/ekfslam/StreamingJob.java
+++ src/main/java/edu/tuberlin/dbpro/ws19/ekfslam/StreamingJob.java
@@ -18,7 +18,18 @@
 
 package edu.tuberlin.dbpro.ws19.ekfslam;
 
+import edu.tuberlin.dbpro.ws19.ekfslam.data.KeyedDataPoint;
+import edu.tuberlin.dbpro.ws19.ekfslam.sinks.InfluxDBSinkGPS;
+import edu.tuberlin.dbpro.ws19.ekfslam.sinks.InfluxDBSinkSensor;
+import functions.DiskreteKalmanFilter;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Query;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -38,6 +49,25 @@ public class StreamingJob {
 		// set up the streaming execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+		@SuppressWarnings({"rawtypes", "serial"})
+
+		InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "admin", "admin");
+		String dbName = "DBProTest";
+		influxDB.query(new Query("CREATE DATABASE " + dbName, dbName));
+		influxDB.setDatabase(dbName);
+
+		//KeyedDataPoint<latitude>
+		DataStream<KeyedDataPoint> sensorData = env.readTextFile("src/main/resources/time_steering_speed_aa3_dr.csv")
+				.map(new StreamingJob.ParseData());
+
+		sensorData.print();
+
+		DataStream<KeyedDataPoint> sensorDataFiltered = sensorData
+				.keyBy("key")
+				.flatMap(new DiskreteKalmanFilter());
+
+		sensorDataFiltered.addSink(new InfluxDBSinkSensor<>("DBProTest", "sensorTest"));
 		/*
 		 * Here, you can start creating your execution plan for Flink.
 		 *
@@ -59,6 +89,27 @@ public class StreamingJob {
 		 */
 
 		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
+		env.execute("EKF-SLAM");
+	}
+
+	private static class ParseData extends RichMapFunction<String, KeyedDataPoint> {
+		private static final long serialVersionUID = 1L;
+
+
+		@Override
+		public KeyedDataPoint<Tuple2> map(String record) {
+			//String rawData = record.substring(1, record.length() - 1);
+			String[] data = record.split(",");
+
+			// the data look like this...
+			// timestamp, latitude, longitude
+
+			//get timestamp, lat and lon from data
+			//store lat, lan in Tuple2<Double, Double>
+			long timestamp = Long.valueOf(data[0]);
+			Tuple2 latLong = new Tuple2<Double, Double>(Double.valueOf(data[1]), Double.valueOf(data[2]));
+			Object d = latLong.getField(0);			//create and return Datapoint with latitude
+			return new KeyedDataPoint<Tuple2>("sensordata",timestamp, latLong);
+		}
 	}
 }
---
+++ src/main/java/edu/tuberlin/dbpro/ws19/ekfslam/sinks/InfluxDBSinkSensor.java
@@ -0,0 +1,68 @@
+package edu.tuberlin.dbpro.ws19.ekfslam.sinks;
+
+import edu.tuberlin.dbpro.ws19.ekfslam.data.DataPoint;
+import edu.tuberlin.dbpro.ws19.ekfslam.data.KeyedDataPoint;
+import org.apache.flink.api.common.functions.RuntimeContext;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+
+import java.util.concurrent.TimeUnit;
+
+public class InfluxDBSinkSensor<T extends DataPoint<? extends Number>> extends RichSinkFunction<T> {
+
+    private transient InfluxDB influxDB = null;
+    private static String dataBaseName;
+    private static String field0Name = "latitude";
+    private static String field1Name = "longitude";
+    private static String field2Name = "steering";
+    private String measurement;
+
+    /**
+     * Provide Database name and measurement (series) name
+     * @param dbName
+     * @param measurement
+     */
+    public InfluxDBSinkSensor(String dbName, String measurement){
+        this.dataBaseName = dbName;
+        this.measurement = measurement;
+    }
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        influxDB = InfluxDBFactory.connect("http://localhost:8086", "admin", "admin");
+        influxDB.createDatabase(dataBaseName);
+        influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+    }
+
+    @Override
+    public void invoke(DataPoint dataPoint, Context context) throws Exception {
+        Tuple3 value =  (Tuple3) dataPoint.getValue();
+        Point.Builder builder = Point.measurement(measurement)
+                .time(dataPoint.getTimeStampMs(), TimeUnit.MILLISECONDS)
+                .addField(field0Name, (Double) value.f0)
+                .addField(field1Name, (Double) value.f1)
+                .addField(field2Name, (Double) value.f2);
+
+        if(dataPoint instanceof KeyedDataPoint){
+            builder.tag("key", ((KeyedDataPoint) dataPoint).getKey());
+
+        }
+
+        Point p = builder.build();
+
+        influxDB.write(dataBaseName, "autogen", p);
+    }
+}
