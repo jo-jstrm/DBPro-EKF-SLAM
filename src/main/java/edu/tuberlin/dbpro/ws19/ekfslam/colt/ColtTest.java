7f751532abdd96b7a88a4c8b0d5a8f1acccd3511
MarkParanskij <mark@germanautolabs.com>
2019-11-29 16:41:24 +0100

    Added Colt Library and 2D matrix test

--- src/main/java/edu/tuberlin/dbpro/ws19/ekfslam/colt/ColtTest.java
+++ src/main/java/edu/tuberlin/dbpro/ws19/ekfslam/colt/ColtTest.java
@@ -1,7 +1,15 @@
 package edu.tuberlin.dbpro.ws19.ekfslam.colt;
 
+import cern.colt.matrix.DoubleMatrix2D;
+import cern.colt.matrix.impl.DenseDoubleMatrix2D;
+
 public class ColtTest {
     public static void main(String[] args){
         System.out.println("Chill :)");
+        DoubleMatrix2D matrix;
+        matrix = new DenseDoubleMatrix2D(3,4);
+//matrix = new SparseDoubleMatrix2D(3,4); // has same interface
+//matrix = new RCDoubleMatrix2D(3,4); // has same interface
+        System.out.println(matrix);
     }
 }
